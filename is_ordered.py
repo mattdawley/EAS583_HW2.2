9554bcebc09b976cc25bce04c297cbf6d13858fe
Matt Dawley <dawley@gmail.com>
2024-01-22 15:16:01 -0700

    first attempt

--- is_ordered.py
+++ is_ordered.py
@@ -1,9 +1,12 @@
+import math
+
 from web3 import Web3
 import random
 import json
 
 
-rpc_url = "http://localhost:8545" #Set this to a node that you can connect to (e.g. an Alchemy node)
+#rpc_url = "http://localhost:8545" #Set this to a node that you can connect to (e.g. an Alchemy node)
+rpc_url = "https://mainnet.infura.io/v3/7d971d5755d142b49329f02a8bfd5d72"
 w3 = Web3(Web3.HTTPProvider(rpc_url))
 
 if w3.is_connected():
@@ -26,15 +29,33 @@ else:
 """
 def is_ordered_block(block_num):
     block = w3.eth.get_block(block_num)
-    ordered = False
+    ordered = True
+    prev = math.inf
     
     #YOUR CODE HERE
+    transactions = block.transactions
+    for tx in transactions:
+        curr_tx = w3.eth.get_transaction(tx)
+        if(curr_tx.type != 2):
+            if curr_tx.gasPrice > prev:
+                return False
+            else:
+                prev = curr_tx.gasPrice
+        else:
+            total_fee = min(curr_tx.maxPriorityFeePerGas + curr_tx.baseFeePerGas, curr_tx.maxFeePerGas)
+            if total_fee > prev:
+                return False
+            else:
+                prev = total_fee
+        #print(w3.eth.get_transaction(tx).type)
 
     return ordered
 
+print(is_ordered_block(12965000))
+
 """
 	This might be useful for testing
-"""
+
 if __name__ == "__main__":
 	latest_block = w3.eth.get_block_number()
 
@@ -59,4 +80,4 @@ if __name__ == "__main__":
 			print( f"Block {block_num} is ordered" )
 		else:
 			print( f"Block {block_num} is ordered" )
-
+"""
